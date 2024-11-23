84f26bae9e71a8c0ffed277de2a689401b91c0f9
SbWereWolf <ulfnew@gmail.com>
2024-11-23 10:28:07 +0500

    Add web API layout and one end point layout

---
+++ src/api-clinet/ApiLayout.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { reactive } from 'vue'
+
+import Accounts from './end-point-client/accounts/ApiEndPoint.vue'
+
+const data = reactive({
+    location: 'http://localhost:8000',
+    accounts: '/Accounts',
+})
+</script>
+
+<template>
+    <div>
+        <input v-model="data.location" />
+    </div>
+    <div>
+        <h1>Картины на заказ API web client</h1>
+        <h2>Home</h2>
+        <h2>Accounts</h2>
+        <details>
+            <summary>Методы</summary>
+            <Accounts v-model:api-location="data.location" v-model:end-point="data.accounts" />
+        </details>
+        <h2>Visitors</h2>
+        <h2>Artists</h2>
+        <h2>Vouchers</h2>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+}
+
+h2 {
+    font-size: 1.2rem;
+}
+
+@media (min-width: 1024px) {
+    .greetings h1,
+    .greetings h2 {
+        text-align: left;
+    }
+}
+</style>
---
+++ src/api-clinet/end-point-client/accounts/ApiEndPoint.vue
@@ -0,0 +1,15 @@
+<script setup>
+import LoginForm from './LoginForm.vue'
+
+const apiLocation = defineModel('apiLocation', { default: 'API server host name with protocol' })
+const endPoint = defineModel('endPoint', { default: 'API call end point' })
+</script>
+
+<template>
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+    <LoginForm v-model:api-location="apiLocation" v-model:end-point="endPoint" method="POST" />
+</template>
