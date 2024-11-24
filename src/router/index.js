7db6bfee39e25f83ff9d9f2cba7a8a5401f52bc8
SbWereWolf <ulfnew@gmail.com>
2024-11-24 14:43:11 +0500

    Remove TheWelcome component

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import ApiLayout from '../api-clinet/ApiLayout.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-        {
             path: '/api-web-client',
             name: 'web-client',
             component: ApiLayout,
         },
         {
-            path: '/about',
+            path: '/',
             name: 'about',
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
